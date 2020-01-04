6ab2bbc67c49ceb4c421ab9a2836536ad6547685
junior pires alide <junior-pires007@live.com>
2020-01-04 09:23:25 +0200

    Add project files.

---
+++ customLoading/customLoading/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using customLoading.Services;
+using customLoading.Views;
+
+namespace customLoading
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ customLoading/customLoading/Views/AboutPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Rg.Plugins.Popup.Extensions;
+using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Rg.Plugins.Popup.Services;
+using System.Threading.Tasks;
+
+namespace customLoading.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var page = new popup();
+            await PopupNavigation.Instance.PushAsync(page);
+            await Task.Delay(3000);
+            await PopupNavigation.Instance.PopAsync();
+        }
+    }
+}
